15933ea9199fb97fce47d51155171fbdcefc86de
Andrés Bercovich <andresbercovich@hotmail.com>
2020-12-03 13:04:31 -0300

    Update Main.ino

--- Main/Main.ino
+++ Main/Main.ino
@@ -2,34 +2,50 @@
 #include <MagAlpha.h>
 
 
-
-
 QTRSensors qtr;
 MagAlpha magAlpha;
 uint16_t sensorValues[6];
+
+int menucounts = 0;
+
+int encoder_offset = 0;
+
+
+
+int buttonPushCounter = 0;
+
+
+# define CS_ENC_L 2
+# define CS_ENC_R 3
+# define TB6612_AIN1  4
+# define TB6612_PWMA  5
+# define TB6612_PWMB  6
+# define TB6612_AIN2  7
+# define TB6612_BIN1  8
+# define TB6612_BIN2  9
+# define BUZZER 10
+# define LEDON  11
+# define HR A0
+# define HL A7
+
+
 
 
 
 void setup()
 {
 
-  pinMode(13, OUTPUT);
   Serial.begin(115200);
   encoders_init();
-  calibration();
+  beep_1();
 
 
 }
 
 void loop() {
 
-
-  uint16_t L = magAlpha.readAngleRaw16();
-
-  uint16_t position = qtr.readLineWhite(sensorValues);
-
-
-  Serial.println(L);
-  delay(20);
+  boton_l1();
+  menu_enc_count();
+  wheel_scroll_1();
 
 }
